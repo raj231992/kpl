44c8a447186e1e7749fd538b71f1cb83ac7e50a9
Raj <nagarajum@cse.iitb.ac.in>
2017-02-12 16:04:46 +0530

    latest entries

--- teams/urls.py
+++ teams/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import Team_Display
+from .views import Team_Display,Team_Players
 
 urlpatterns = [
     url(r'^$', Team_Display.as_view()),
+    url(r'^players/(?P<pk>[0-9]+)/', Team_Players.as_view(),name='players'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Team
+from auctions.models import Sold_Player
 # Create your views here.
 
 
@@ -11,3 +12,15 @@ class Team_Display(View):
             'teams':teams
         }
         return render(request,'teams.html',context)
+
+class Team_Players(View):
+    def get(self,request,pk):
+        team = Team.objects.get(pk=pk)
+        manager = team.manager
+        players = Sold_Player.objects.filter(team=team).order_by('player__name')
+        context = {
+            'team':team,
+            'manager':manager,
+            'players':players,
+        }
+        return render(request,'team_players.html',context)
