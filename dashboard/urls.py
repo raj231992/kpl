fdc2eeb8b94b2d0a56b66b2598c688fbc3b7e11b
Raj <nagarajum@cse.iitb.ac.in>
2017-02-12 16:18:02 +0530

    registration closed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,6 @@ from .views import Home,Rules,Register,Organizers,Players
 urlpatterns = [
     url(r'^$', Home.as_view(),name='home'),
     url(r'^rules/$', Rules.as_view(),name='rules'),
-    url(r'^register/$', Register.as_view(),name='register'),
     url(r'^organizers/$', Organizers.as_view(),name='organizers'),
     url(r'^players/$', Players.as_view(),name='players'),
 ]
